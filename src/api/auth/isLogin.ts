efc63a2135e9823889c19a8c66e219745879f72a
송현우 <shw9052@naver.com>
2024-03-07 23:40:11 +0900

    Add login api

--- src/api/auth/isLogin.ts
+++ src/api/auth/isLogin.ts
@@ -1,14 +1,62 @@
-import axios, { Axios } from "axios";
-import { useEffect } from "react";
+import axios from "axios";
+import isRefreshToken from "../common/isRefreshToken";
+import { useRouter } from "next/navigation";
 
-export default function Admin() {
-    const api = axios.create({
-        baseURL: ,
+const baseURL = process.env.NEXT_PUBLIC_SERVER_URL;
+const api = axios.create({
+  baseURL: baseURL,
+});
+
+export default async function isLogin(email: string, pw: string) {
+  try {
+    const response = await api.post("/auth/signIn", {
+      refreshToken: email,
+      password: pw,
+    });
+
+    if (response.status === 200) {
+      localStorage.setItem("accessToken", response.data.accessToken);
+      localStorage.setItem(
+        "refreshToken",
+        "Bearer " + response.data.refreshToken
+      );
+      const { data } = await api.get("/user", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
       });
-      
-    function checkLogin() {
-        try{
-            const response = await api.post("auth/signIn");
-        }
+      if (data.role === "ROLE_ADMIN") {
+        alert("로그인에 성공하셨습니다.");
+        useRouter().push("/inquiry");
+      } else {
+        alert("관리자 계정이 아닙니다.");
+      }
+    } else {
+      alert("등록되지 않은 회원입니다.");
     }
+  } catch (error: any) {
+    if (error.response && error.response.status === 403) {
+      alert("접근 권한이 없습니다.");
+    } else if (error.response && error.response.status === 401) {
+      try {
+        await isRefreshToken();
+
+        const response = await api.post("/auth/signIn", {
+          email: email,
+          password: pw,
+        });
+
+        if (response.status === 200) {
+          localStorage.setItem("accessToken", response.data.accessToken);
+          localStorage.setItem("refreshToken", response.data.refreshToken);
+        } else {
+          alert("등록되지 않은 회원입니다.");
+        }
+      } catch (refreshError) {
+        alert("토큰 갱신 및 로그인 실패");
+      }
+    } else {
+      alert("이메일 또는 비밀번호가 일치하지 않습니다.");
+    }
+  }
 }
