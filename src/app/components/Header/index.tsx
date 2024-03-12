8d08699a39512c67fe6f975ffbe170fd68ca22c5
송현우 <shw9052@naver.com>
2024-03-12 11:20:59 +0900

    Add header modal onclick

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -2,8 +2,15 @@
 
 import { MisoIcon } from "@/assets/svg";
 import * as S from "./style";
+import { useState } from "react";
+import Modal from "../modal";
 
 const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <S.HeaderWrapper>
       <S.MenuContainer>
@@ -11,8 +18,18 @@ const Header = () => {
           <MisoIcon />
           MISO
         </S.IconBox>
-        <S.LogOutButton>로그아웃</S.LogOutButton>
+        <S.LogOutButton onClick={openModal}>로그아웃</S.LogOutButton>
       </S.MenuContainer>
+      <Modal
+        isOpen={isModalOpen}
+        closeModal={closeModal}
+        title={"로그아웃 하시겠습니까?"}
+        content1={"정말로 로그아웃 하시겠습니까?"}
+        content2={""}
+        button={"로그아웃"}
+        id={""}
+        mainText={""}
+      />
     </S.HeaderWrapper>
   );
 };
