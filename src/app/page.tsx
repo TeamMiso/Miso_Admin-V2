0979176479192491090af8abf9764684d96a74c2
송현우 <shw9052@naver.com>
2024-03-14 15:57:45 +0900

    Update inquiry page type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,17 @@ import { useEffect, useState } from "react";
 import isCheckToken from "@/api/common/isCheckToken";
 import { useRouter } from "next/navigation";
 import { InquiryItem } from "@/components";
+import { InquiryItemType } from "@/types";
 
 export default function InquiryPage() {
   const router = useRouter();
-  const [inquiryData, setInquiryData] = useState<any>(null);
+  const [inquiryItemList, setInquiryItemList] = useState<InquiryItemType[]>([]);
 
   useEffect(() => {
     async function fetchData() {
       try {
         const data = await isCheckToken(router);
-        setInquiryData(data);
+        setInquiryItemList(data.inquiryList);
       } catch (error) {
         console.error("조회 데이터를 가져오는 중 오류 발생:", error);
       }
@@ -32,7 +33,16 @@ export default function InquiryPage() {
           <S.Title>문의목록</S.Title>
           <S.FilterButton>필터</S.FilterButton>
         </S.InquiryMenu>
-        <InquiryItem />
+        {inquiryItemList.length > 0 &&
+          inquiryItemList.map((data) => (
+            <InquiryItem 
+            id={data.id} 
+            inquiryDate={data.inquiryDate}
+            title={data.title}
+            imageUrl={data.imageUrl}
+            inquiryStatus={data.inquiryStatus}
+            />
+          ))}
       </S.InquiryList>
     </S.InquiryWrapper>
   );
