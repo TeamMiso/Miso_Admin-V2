6dd9b3fda8faa65c759470394c6da7ad039217b5
송현우 <shw9052@naver.com>
2024-03-14 20:44:34 +0900

    Add filter component publish

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,7 +1,31 @@
+import { FilterProps } from "@/types";
 import * as S from "./style";
+import { match } from "ts-pattern";
 
-const Filter = () => {
-  return <S.FilterWrapper></S.FilterWrapper>;
+const Filter = ({ isOpen }: FilterProps) => {
+  return (
+    <S.FilterWrapper
+      display={match(isOpen)
+        .with(true, () => "block")
+        .otherwise(() => "none")}
+    >
+      <S.FilterLayout>
+        <S.FilterContainer>
+          <S.Title>게시글 상태</S.Title>
+          <S.CheckMenu>
+            <div>
+              <span>검토 중</span>
+              <S.CheckBox type="checkbox" />
+            </div>
+            <div>
+              <span>답변 완료</span>
+              <S.CheckBox type="checkbox" />
+            </div>
+          </S.CheckMenu>
+        </S.FilterContainer>
+      </S.FilterLayout>
+    </S.FilterWrapper>
+  );
 };
 
 export default Filter;
--- src/components/Filter/style.ts
+++ src/components/Filter/style.ts
@@ -1,3 +1,51 @@
 import styled from "styled-components";
 
-export const FilterWrapper = styled.div``;
+export const FilterWrapper = styled.div<{ display: string }>`
+  right: 0;
+  position: fixed;
+  padding-top: 7.5rem;
+  width: calc(50% - 40rem);
+  height: 100vh;
+  display: ${({ display }) => display};
+`;
+
+export const FilterLayout = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const FilterContainer = styled.div`
+  border: 0.0625rem solid ${({ theme }) => theme.color.gray["700"]};
+  background-color: ${({ theme }) => theme.color.white};
+  border-radius: 0.5rem;
+  width: 10rem;
+  padding: 0.5rem;
+`;
+
+export const Title = styled.span`
+  ${({ theme }) => theme.typo.text_md.semibold};
+`;
+
+export const CheckMenu = styled.div`
+  div {
+    flex-wrap: wrap;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 1rem;
+  }
+`;
+
+export const CheckBox = styled.input`
+  background-color: ${({ theme }) => theme.color.gray["700"]};
+  border-radius: 0.5rem;
+  width: 1.5rem;
+  height: 1.5rem;
+  appearance: none;
+  cursor: pointer;
+  &:checked {
+    background-color: ${({ theme }) => theme.color.main};
+    background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z'/%3e%3c/svg%3e");
+  }
+`;
