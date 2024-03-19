78e42afa3264c6b0954583adea0723d5ee4fea23
송현우 <shw9052@naver.com>
2024-03-20 00:49:30 +0900

    Update inquiryitemtypes expoart naming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,13 @@ import { useEffect, useState } from "react";
 import isCheckToken from "@/api/common/isCheckToken";
 import { useRouter } from "next/navigation";
 import { InquiryItem } from "@/components";
-import { InquiryItemType } from "@/types";
+import { InquiryItemTypes } from "@/types";
 
 export default function InquiryPage() {
   const router = useRouter();
-  const [inquiryItemList, setInquiryItemList] = useState<InquiryItemType[]>([]);
+  const [inquiryItemList, setInquiryItemList] = useState<InquiryItemTypes[]>(
+    []
+  );
   const [isFilterOpen, setIsFilterOpen] = useState(false);
   const [isReviewChecked, setIsReviewChecked] = useState(true);
   const [isAnswerChecked, setIsAnswerChecked] = useState(true);
