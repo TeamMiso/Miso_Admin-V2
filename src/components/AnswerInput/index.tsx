66d99b5ef45dcfdc09bc41dc2f6334dbb1abf5f2
송현우 <shw9052@naver.com>
2024-03-19 23:27:55 +0900

    Add use answer input modal

--- src/components/AnswerInput/index.tsx
+++ src/components/AnswerInput/index.tsx
@@ -1,11 +1,34 @@
+import { ChangeEvent, useState } from "react";
 import * as S from "./style";
+import { Modal } from "@/components";
+import { AnswerInputProps } from "@/types";
 
-const AnswerInput = () => {
+const AnswerInput = ({ id }: AnswerInputProps) => {
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [mainText, setMainText] = useState("");
+
+  const handleText = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setMainText(e.target.value);
+  };
+
   return (
     <S.AnswerInputWrapper>
       <S.Title>답변하기</S.Title>
-      <S.AnswerTextarea placeholder="답변 내용 쓰기" />
-      <S.AnswerButton>답변하기</S.AnswerButton>
+      <S.AnswerTextarea placeholder="답변 내용 쓰기" onChange={handleText} />
+      <S.AnswerButton onClick={openModal}>답변하기</S.AnswerButton>
+      <Modal
+        isOpen={isModalOpen}
+        closeModal={closeModal}
+        title={"답변을 게시할까요?"}
+        content1={"한 번 답변하면 수정할 수 없어요!"}
+        content2={"신중히 답변해 보아요 :)"}
+        button={"답변 게시"}
+        id={id}
+        mainText={mainText}
+      />
     </S.AnswerInputWrapper>
   );
 };
