460c60773c875a4fed42ac6b81f681dee5e9f69e
송현우 <shw9052@naver.com>
2024-03-22 15:33:16 +0900

    Update login backround

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -40,41 +40,43 @@ export default function LoginPage() {
 
   return (
     <S.LoginPageWrapper imgUrl={Background}>
-      <S.LoginContainer>
-        <S.LoginWrapper>
-          <S.TitleContainer>
-            <S.Title>미소</S.Title>
-            <S.SemiTitle>환경을 웃음으로 바꾸다 :)</S.SemiTitle>
-          </S.TitleContainer>
-          <S.InputContainer>
-            <S.Input
-              isStatus={!emailValid && email.length > 0}
-              onChange={handleEmail}
-              placeholder="이메일"
-              type="text"
-            />
-            <S.ErrorMessageWrap>
-              {!emailValid && email.length > 0 && (
-                <span>올바른 이메일을 입력해주세요.</span>
-              )}
-            </S.ErrorMessageWrap>
-            <S.Input
-              isStatus={!pwValid && pw.length}
-              onChange={handlePassword}
-              placeholder="비밀번호"
-              type="password"
-            />
-            <S.ErrorMessageWrap>
-              {!pwValid && pw.length > 0 && (
-                <span>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</span>
-              )}
-            </S.ErrorMessageWrap>
-          </S.InputContainer>
-          <S.LoginButton onClick={() => handleLoginClick(router)}>
-            로그인
-          </S.LoginButton>
-        </S.LoginWrapper>
-      </S.LoginContainer>
+      <S.BackgroundCover>
+        <S.LoginContainer>
+          <S.LoginWrapper>
+            <S.TitleContainer>
+              <S.Title>미소</S.Title>
+              <S.SemiTitle>환경을 웃음으로 바꾸다 :)</S.SemiTitle>
+            </S.TitleContainer>
+            <S.InputContainer>
+              <S.Input
+                isStatus={!emailValid && email.length > 0}
+                onChange={handleEmail}
+                placeholder="이메일"
+                type="text"
+              />
+              <S.ErrorMessageWrap>
+                {!emailValid && email.length > 0 && (
+                  <span>올바른 이메일을 입력해주세요.</span>
+                )}
+              </S.ErrorMessageWrap>
+              <S.Input
+                isStatus={!pwValid && pw.length}
+                onChange={handlePassword}
+                placeholder="비밀번호"
+                type="password"
+              />
+              <S.ErrorMessageWrap>
+                {!pwValid && pw.length > 0 && (
+                  <span>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</span>
+                )}
+              </S.ErrorMessageWrap>
+            </S.InputContainer>
+            <S.LoginButton onClick={() => handleLoginClick(router)}>
+              로그인
+            </S.LoginButton>
+          </S.LoginWrapper>
+        </S.LoginContainer>
+      </S.BackgroundCover>
     </S.LoginPageWrapper>
   );
 }
