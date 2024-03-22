55e617dc8617b03037159964623d228569d69eb5
송현우 <shw9052@naver.com>
2024-03-22 10:47:48 +0900

    Update filter icon fill

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,3 +1,4 @@
 export { default as MisoIcon } from "./MisoIcon";
 export { default as Check } from "./Check";
 export { default as FilterIcon } from "./FilterIcon";
+export { default as SmallCheck } from "./SmallCheck";
