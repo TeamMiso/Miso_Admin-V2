2d5a4f987904e35847d6097d5692159e99292689
송현우 <shw9052@naver.com>
2024-05-02 11:06:12 +0900

    Update header router path

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,19 +4,21 @@ import { MisoIcon } from "@/assets/svg";
 import * as S from "./style";
 import { useState } from "react";
 import { Modal } from "@/components";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
+  const router = useRouter();
+
   return (
     <S.HeaderWrapper>
-      <S.MenuContainer>
+      <S.MenuContainer onClick={() => router.push("/")}>
         <S.IconBox>
           <MisoIcon />
-           <span>MISO</span>
+          <span>MISO</span>
         </S.IconBox>
         <S.LogOutButton onClick={openModal}>로그아웃</S.LogOutButton>
       </S.MenuContainer>
